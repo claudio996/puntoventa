36a09e9baac22fd07e26269087800d6856c3bc68
Suminona99 <claudioantonio.retamal@gmail.com>
2021-09-11 01:42:40 -0300

    working entities products function calcsubtotal()

--- app/Entities/ProductosEntity.php
+++ app/Entities/ProductosEntity.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace App\Entities;
-
+use App\Models\TemporalCompraModel;
 use CodeIgniter\Entity\Entity;
 
 class ProductosEntity extends Entity
 {
+    protected $temporalCompra;
+    public function __construct()
+    {
+        $this->temporalCompra = new TemporalCompraModel();
+    }
+
     protected $datamap = [];
     protected $dates   = [
         'created_at',
@@ -13,4 +19,10 @@ class ProductosEntity extends Entity
         'deleted_at',
     ];
     protected $casts   = [];
+
+    public function calsubtotal($cantidad)
+    {
+        $subtotal =  ($cantidad) * $this->attributes['precio_compra'];
+        return $subtotal;
+    }
 }
